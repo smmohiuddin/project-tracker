df203f122bbd8f94bd6217974490c18af23ee620
smmohiuddin <s.mohiuddin64@gmail.com>
2017-06-08 12:46:32 +0600

    Web configuration added

---
+++ src/main/java/com/genweb2/projecttracker/configurations/WebConfiguration.java
@@ -0,0 +1,12 @@
+package com.genweb2.projecttracker.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by shakil on 6/8/17.
+ */
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+}
