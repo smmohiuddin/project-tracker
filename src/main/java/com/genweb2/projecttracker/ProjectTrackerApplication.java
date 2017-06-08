ec88ab78350db3c9ed4b27e7a6dc1b99efbf003d
smmohiuddin <s.mohiuddin64@gmail.com>
2017-06-08 12:40:49 +0600

    Spring boot starter project

---
+++ src/main/java/com/genweb2/projecttracker/ProjectTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.genweb2.projecttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTrackerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/genweb2/projecttracker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.genweb2.projecttracker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectTrackerApplication.class);
+	}
+
+}
