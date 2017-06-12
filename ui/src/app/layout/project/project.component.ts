66038b39f433d008f69a7a74e5d6bb400376d179
smmohiuddin <s.mohiuddin64@gmail.com>
2017-06-12 17:28:41 +0600

    UI list and modal prepared.

--- ui/src/app/layout/project/project.component.ts
+++ ui/src/app/layout/project/project.component.ts
@@ -1,25 +1,52 @@
 import {Component, OnInit} from '@angular/core';
 
 import {ProjectService} from '../../shared/services/project.service';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+
 import {Project} from '../../shared/models/project';
+
 
 @Component({
     selector: 'app-projects',
-    providers:[ProjectService],
+    providers:[ProjectService, NgbModal],
     templateUrl: './project.component.html',
     styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
-    projects: Project[];
-    errorMessage: string = "";
 
-    constructor(private projectService: ProjectService) {}
+    selectedProject : Project;
+    projects: Project[];
+    closeResult: string;
+    modal: NgbModalRef;
+
+    constructor(private projectService: ProjectService, private modalService: NgbModal) {};
+
+    open(content, project) {
+        this.selectedProject = Object.assign({}, project);
+        this.modal = this.modalService.open(content);
+    };
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return  `with: ${reason}`;
+        }
+    };
+
+    save() : void {
+        console.log(this.selectedProject);
+        this.closeModal("project info saved")
+    };
+
+    closeModal(reason: any) {
+        this.modal.dismiss(reason);
+    }
 
     getProjects() : void {
-        this.projectService.getProjects().subscribe(
-            projects => this.projects = projects,
-            error =>  this.errorMessage = <any>error
-        );
+        this.projects = this.projectService.getProjects()
     }
 
     ngOnInit() {
--- ui/src/app/shared/services/project.service.ts
+++ ui/src/app/shared/services/project.service.ts
@@ -11,21 +11,45 @@ import {Project} from '../models/project';
 @Injectable()
 export class ProjectService {
 
-    private projectUrl = 'http://localhost:8080/projects';
+    // private projectUrl = 'http://localhost:8080/projects';
+    private projects: Project[] = [
+        {
+            projectID: 1,
+            projectName: "FAMS (ERD)",
+            projectDesc: "A project from finance ministry of Bangladesh",
+            startDate: new Date(),
+            actualStartDate: new Date(),
+            endDate: new Date(),
+            actualEndDate: new Date()
+        },
+        {
+            projectID: 2,
+            projectName: "eMobile Court",
+            projectDesc: "A project form A2I",
+            startDate: new Date(),
+            actualStartDate: new Date(),
+            endDate: new Date(),
+            actualEndDate: new Date()
+        },
+    ];
 
-    constructor(private http: Http) {}
+    constructor() {}
 
-    private getHeaders(){
+    /*private getHeaders(){
         let headers = new Headers();
         headers.append('Accept', 'application/json');
         return headers;
-    }
+    }*/
 
-    getProjects(): Observable<Project[]> {
+    /*getProjects(): Observable<Project[]> {
         return this.http.get(this.projectUrl, {headers: this.getHeaders()}).map(this.mapData);
+    }*/
+
+    getProjects(): Project[] {
+        return this.projects;
     }
 
-    private mapData(response: Response) : Project[] {
+    /*private mapData(response: Response) : Project[] {
         return response.json().results.map(this.toProject)
     }
 
@@ -41,5 +65,5 @@ export class ProjectService {
         });
         console.log('Parsed project:', project);
         return project;
-    }
+    }*/
 }
