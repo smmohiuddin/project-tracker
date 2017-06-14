462076d9e66e3d10a67ef224e03bb2acee704d1f
smmohiuddin <s.mohiuddin64@gmail.com>
2017-06-14 15:30:46 +0600

    project dropdown populated in epic

--- ui/src/app/layout/epic/epic.component.ts
+++ ui/src/app/layout/epic/epic.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Project} from "../../shared/models/project";
+import {ProjectService} from "../../shared/services/index";
 
 @Component({
-  selector: 'app-epic',
-  templateUrl: './epic.component.html',
-  styleUrls: ['./epic.component.scss']
+    selector: 'app-epic',
+    providers: [ProjectService],
+    templateUrl: './epic.component.html',
+    styleUrls: ['./epic.component.scss']
 })
 export class EpicComponent implements OnInit {
 
-  constructor() { }
+    errorMessage: string = "";
+    projects: Project[] = [];
 
-  ngOnInit() {
-  }
+    constructor(private projectService: ProjectService) {}
+
+    getProjects() : void {
+        this.projectService.getProjects().subscribe(
+            projects => this.projects = projects,
+            error => this.errorMessage = <any> error
+        );
+    }
+
+    ngOnInit() {
+        this.getProjects();
+    }
 
 }
