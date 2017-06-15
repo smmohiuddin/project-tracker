1f413b1cf82f01a0a99412a9acd98bc9930b2911
smmohiuddin <s.mohiuddin64@gmail.com>
2017-06-15 13:58:55 +0600

    Task component and module have been generated.

--- ui/src/app/layout/layout-routing.module.ts
+++ ui/src/app/layout/layout-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
             { path: 'projects', loadChildren: './project/project.module#ProjectModule' },
             { path: 'epics', loadChildren: './epic/epic.module#EpicModule' },
             { path: 'stories', loadChildren: './story/story.module#StoryModule' },
+            { path: 'tasks', loadChildren: './task/task.module#TaskModule' },
 
             /*{ path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
             { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
