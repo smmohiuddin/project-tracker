f549fbcde249450959e4eb261c924d7af060257e
smmohiuddin <s.mohiuddin64@gmail.com>
2017-06-19 03:01:27 +0600

    Project Resource filter added.

--- ui/src/app/layout/project-resource/project-resource.component.ts
+++ ui/src/app/layout/project-resource/project-resource.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProjectService, ResourceService} from "../../shared/services/index";
+import {Project, Resource} from "../../shared/models/index";
 
 @Component({
-  selector: 'app-project-resource',
-  templateUrl: './project-resource.component.html',
-  styleUrls: ['./project-resource.component.scss']
+    selector: 'app-project-resource',
+    providers: [ProjectService, ResourceService],
+    templateUrl: './project-resource.component.html',
+    styleUrls: ['./project-resource.component.scss']
 })
 export class ProjectResourceComponent implements OnInit {
 
-  constructor() { }
+    projects: Project[];
+    errorMessage: string;
+    selectedProject: Project;
+    selectedResource: Resource;
+    resources: Resource[];
 
-  ngOnInit() {
-  }
+    constructor(private projectService: ProjectService, private resourceService: ResourceService) {
+    }
+
+    getProjects(): void {
+        this.projectService.getProjects().subscribe(
+            projects => this.projects = projects,
+            error => this.errorMessage = <any> error
+        );
+    }
+
+    getResources(): void {
+        this.resourceService.getResources().subscribe(
+            resources => this.resources = resources,
+            error => this.errorMessage = <any> error
+        );
+    }
+
+    ngOnInit() {
+        this.getProjects();
+        this.getResources();
+    }
 
 }
